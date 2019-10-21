286e13965149a7a8c9ea411c3f087ee3e7ae1900
DNikolova93 <doroteya_nikolova@epam.com>
2019-10-21 12:23:13 +0300

    chore: Bump to version 2

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Version 2 (2019-10-21)
+
+## Features
+* Added index.js script ([7fd764](https://github.com/DNikolova93/changelog-generator/commit/7fd7641844a3676845dc8b7393c6fffe7d3bb360))
+
+## Chores
+* Initial commit for changelog generator ([c2b98d](https://github.com/DNikolova93/changelog-generator/commit/c2b98db703ce25227de0222c5eba504d8e0b1378))
+
---
+++ index.js
@@ -0,0 +1,70 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+const output = execSync(`git log --format=%B%H----DELIMITER----`).toString('utf-8');
+
+// adding delimiter string to help us to splut the string
+// filtered the commit if it doesn't have SHA hash
+const commitsArray = output.split('----DELIMITER----\n').map(commit => {
+  const [message, sha] = commit.split('\n');
+
+  return { sha, message };
+}).filter(commit => Boolean(commit.sha));
+
+console.log({ commitsArray });
+
+const currentChangelog = fs.readFileSync('./CHANGELOG.md', 'utf-8');
+const currentVersion = Number(require('./package.json').version);
+const newVersion = currentVersion + 1;
+
+let newChangelog = `# Version ${newVersion} (${
+  new Date().toISOString().split('T')[0]
+})\n\n`;
+
+const features = [];
+const chores = [];
+
+commitsArray.forEach(commit => {
+  if (commit.message.startsWith('feature: ')) {
+    features.push(
+      `* ${commit.message.replace('feature: ', '')} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/DNikolova93/changelog-generator/commit/${
+        commit.sha
+      }))\n`
+    );
+  }
+
+  if (commit.message.startsWith('chore: ')) {
+    chores.push(
+      `* ${commit.message.replace('chore: ', '')} ([${commit.sha.substring(
+        0,
+        6
+      )}](https://github.com/DNikolova93/changelog-generator/commit/${
+        commit.sha
+      }))\n`
+    );
+  }
+});
+
+if (features.length) {
+  newChangelog += `## Features\n`;
+  features.forEach(feature => {
+    newChangelog += feature;
+  });
+
+  newChangelog += `\n`;
+}
+
+if (chores.length) {
+  newChangelog += `## Chores\n`;
+  chores.forEach(feature => {
+    newChangelog += feature;
+  });
+
+  newChangelog += `\n`;
+}
+
+// prepend newChangelog to the current one
+fs.writeFileSync('./CHANGELOG.md', `${newChangelog}${currentChangelog}`);
